a650e914c65673498b32d8afd0ce6643a037bc4e
nine96as <ashrafsaid79@gmail.com>
2023-11-01 20:54:51 +0000

    Implement list of users route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,12 @@
 import express from 'express';
 
+import users from '../users.json' assert { type: 'json' };
+
 const router = express.Router();
 
 // all routes in here start with /users
-router.get('/', (req, res) => res.send('Hello! You are at the Fruits page'));
+
+// list of users
+router.get('/', (req, res) => res.send(users));
 
 export default router;
