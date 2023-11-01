1622c7cd895c92da3a5d38fa6c97eb7e4e97bcbe
nine96as <ashrafsaid79@gmail.com>
2023-11-01 20:09:17 +0000

    Switch to ES6 imports, create index.js

--- server/app.js
+++ server/app.js
@@ -1,11 +1,6 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+import express from 'express';
+export const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
 });
