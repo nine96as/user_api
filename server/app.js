d5d282c04a9c670bc5db9d2583da8b8a4110e868
nine96as <ashrafsaid79@gmail.com>
2023-11-01 20:38:08 +0000

    Add /users router

--- server/app.js
+++ server/app.js
@@ -1,23 +1,18 @@
 import cors from 'cors';
 import express from 'express';
 
+import usersRouter from './routes/users.js';
+
 export const app = express();
 
 // middleware section
 app.use(cors());
 app.use(express.json());
 
+// router section
+app.use('/users', usersRouter);
+
 // home page
 app.get('/', (req, res) =>
   res.status(200).send({ message: 'Welcome to the Homepage!' })
 );
-
-// list of users
-
-// specific user page
-
-// create a user
-
-// update a user
-
-// delete a user
