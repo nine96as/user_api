47891ccd30cbf450f2ccd1f8e85de2e606adcca9
nine96as <nine96@skiff.com>
2023-11-01 20:15:10 +0000

    Tweak log output for server startup

--- server/index.js
+++ server/index.js
@@ -2,5 +2,5 @@ import { app } from './app.js';
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server running on port: http://localhost:${port}`);
 });
