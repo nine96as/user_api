14ce2cb95175f6e533db6a90caa307598ad79536
nine96as <nine96@skiff.com>
2023-11-01 21:15:10 +0000

    Implement base version of create fruit route

--- server/routes/users.js
+++ server/routes/users.js
@@ -9,4 +9,20 @@ const router = express.Router();
 // list of users
 router.get('/', (req, res) => res.send(users));
 
+// create a fruit
+router.post('/', (req, res) => {
+  const user = req.body;
+
+  if (!user.firstName || !user.lastName)
+    res
+      .status(422)
+      .send({ error: 'You must provide the first and last name of the user' });
+
+  if (!user.age)
+    res.status(422).send({ error: 'You must provide the age of the user' });
+
+  users.push(user);
+  res.status(201).send(user);
+});
+
 export default router;
