f70e4e0d749bb97a4de29712bac2cfd2d779b004
nine96as <nine96@skiff.com>
2023-11-01 20:18:57 +0000

    Added middlewares, tweaked home page response

--- server/app.js
+++ server/app.js
@@ -1,6 +1,12 @@
+import cors from 'cors';
 import express from 'express';
+
 export const app = express();
 
+// middleware section
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.status(200).send({ message: 'Welcome to the Homepage!' });
 });
