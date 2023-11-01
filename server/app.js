57ded5cfb9e2230a3b6cf8809255d90d0d09716e
nine96as <ashrafsaid79@gmail.com>
2023-11-01 20:22:32 +0000

    Add roadmap for CRUD routes

--- server/app.js
+++ server/app.js
@@ -11,3 +11,13 @@ app.use(express.json());
 app.get('/', (req, res) =>
   res.status(200).send({ message: 'Welcome to the Homepage!' })
 );
+
+// list of users
+
+// specific user page
+
+// create a user
+
+// update a user
+
+// delete a user
