2d4747fa5bfe5a19985da1a6e0c7a069516d0e9d
nine96as <nine96@skiff.com>
2023-11-14 20:47:56 +0000

    Port over logic from controller to create method

--- server/models/User.js
+++ server/models/User.js
@@ -1,14 +1,30 @@
+import { v4 as uuidv4 } from 'uuid';
+
 import { users } from '../data/index.js';
 
 export class User {
   constructor(data) {
-    this.id = data.id;
     this.firstName = data.firstName;
     this.lastName = data.lastName;
     this.age = data.age;
+    this.id = data.id;
   }
 
   static getAll() {
     return users.map((e) => new User(e));
+  }
+
+  static create(data) {
+    if (!data.firstName || !data.lastName)
+      throw new Error('You must provide the first and last name of the user');
+
+    if (!data.age) throw new Error('You must provide the age of the user');
+
+    try {
+      users.push(new User({ ...data, id: uuidv4() }));
+      return users.slice(-1);
+    } catch (e) {
+      throw new Error(e);
+    }
   }
 }
